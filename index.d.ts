bab21e60172f38e63900e4289d3b44e755397c7b
liangxiaojun <fatalxiao@163.com>
2023-02-28 09:59:27 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,31 @@
 export * from 'react-redux';
 
+/**
+ * A hook to access the vivy store's state.
+ * @param nameSpace
+ */
 export function useModelState(nameSpace?: string): any
 
+/**
+ * The action or reducer of Vivy model.
+ */
 export type VivyModelAction = Function
 
+/**
+ * The actions and reducers of Vivy model.
+ */
 export interface VivyModelActions {
     [actionName: string]: VivyModelAction
 }
 
+/**
+ * A hook to access the vivy store's actions and reducers.
+ * @param nameSpace
+ */
 export function useModelActions(nameSpace?: string): VivyModelActions
 
+/**
+ * A hook to access the vivy store's state, actions and reducers.
+ * @param nameSpace
+ */
 export function useModel(nameSpace?: string): [any, VivyModelActions]
